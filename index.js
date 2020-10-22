9589a4b032af9e02c9851a3af3d2353755ca3889
Tri Nguyen <leonguyen0202@gmail.com>
2020-10-23 00:14:47 +0700

    Modify index with console log to test PASSWORD Heroku

--- index.js
+++ index.js
@@ -76,4 +76,8 @@ function initial() {
 // PORT define
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server is running on port: ${PORT}`);
+  
+  console.log(process.env.PASSWORD);
+});
