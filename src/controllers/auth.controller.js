f7163896f8475362a7c2c146edd893302674c2a0
Tri Nguyen <leonguyen0202@gmail.com>
2020-11-15 21:50:04 +0700

    Add avatar data when login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -139,6 +139,7 @@ export const signin = (req, res) => {
         id: user._id,
         username: user.username,
         email: user.email,
+        avatar: user.avatar,
         roles: authorities,
         accessToken: {
           token: token,
