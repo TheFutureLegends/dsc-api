0a70dec0781ad6bb8acde67011b2793f84a273e3
Tri Nguyen <leonguyen0202@gmail.com>
2020-12-01 16:26:49 +0700

    Add post seeder

--- src/seeders/post/post.seeder.js
+++ src/seeders/post/post.seeder.js
@@ -16,22 +16,40 @@ const postSeeder = () => {
 
       const post = new Post({
         title: sentence,
-        slug: slugify(sentence),
+        slug: slugify(sentence.toLowerCase()),
         description: faker.lorem.paragraphs(),
         image: faker.image.avatar(),
         visit: 0,
       });
 
       Category.estimatedDocumentCount((err, categoryCount) => {
-        var random = Math.floor(Math.random() * categoryCount);
+        var randomCategory = Math.floor(Math.random() * categoryCount);
 
         // Again query all users but only fetch one offset by our random #
         Category.findOne()
-          .skip(random)
-          .exec(function (err, category) {
+          .skip(randomCategory)
+          .exec((err, category) => {
             post.category = category._id;
 
-            User.estimatedDocumentCount((err, userCount) => {});
+            User.estimatedDocumentCount((err, userCount) => {
+              var randomUser = Math.floor(Math.random() * userCount);
+              User.findOne()
+                .skip(randomUser)
+                .exec((err, user) => {
+                  post.author = user._id;
+
+                  post.save((err) => {
+                    if (err) {
+                      console.log("Error: ", err);
+                      process.exit();
+                    }
+
+                    console.log(
+                      `\Post with title: ${post.title} is added to collections`
+                    );
+                  });
+                });
+            });
           });
       });
     }
