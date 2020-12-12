c086d515169f34d163851d1c033b0c1613aa6c74
Buithienphuoc <tphuoc1998@gmail.com>
2020-12-12 22:05:18 +0700

    Unit test for : - Category features - View User Profile - Create, Update, View study guides

--- test/category.js
+++ test/category.js
@@ -12,80 +12,83 @@ const Category = db.category;
 
 chai.use(chaiHttp);
 
-describe("Let's run unit test for CRUD Categories !!", () => {
+describe("Let's run unit test for CRUD Categories feature !!", () => {
 
     //Before each test we empty the database
     beforeEach((done) => {
-    Category.remove({}, (err) => {
-      done();
-    });
+        let user = {
+          email: "admin@admin.com",
+          password: "123456789",
+        };
 
-    let user = {
-      email: "admin@admin.com",
-      password: "123456789",
-    };
+        chai
+          .request(app)
+          .post("/api/auth/signin")
+          .send(user)
+          .end((err, res) => {
+            res.should.have.status(200);
 
-    chai
-      .request(app)
-      .post("/api/auth/signin")
-      .send(user)
-      .end((err, res) => {
-        res.should.have.status(200);
+            res.body.should.be.a("object");
 
-        res.body.should.be.a("object");
-
-        token = res.body.accessToken.token;
+            token = res.body.accessToken.token;
       });
-    });
-
-    //after each test we empty the database
-    afterEach((done) => {
-        Category.remove({}, (err) => {
-            done();
-        });
+        done()
     });
 
     /*
     * Test the /GET route
     */
-    describe("/GET post", () => {
-    it("it should GET all the posts", (done) => {
-      chai
-        .request(app)
-        .get("/api/posts")
-        .end((err, res) => {
-          res.should.have.status(200);
+    describe("/GET category", () => {
+    it("it should show (GET) all the categories", (done) => {
+          chai
+            .request(app)
+            .get("/api/categories")
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a("object");
 
-          expect(res.body.posts).to.be.an.instanceof(Array);
+              // res.body.length.should.be.eql(0);
 
-          res.body.should.be.a("object");
-
-          // res.body.length.should.be.eql(0);
-
-          done();
+              done();
+            });
         });
     });
-    });
+    describe("/GET categories by name", () => {
+        it("it should show (GET) posts with the given name", (done) => {
+            const mockCategory = 'Mock'
+            chai
+                .request(app)
+                .get("/api/categories/" + mockCategory)
+                .end((err, res) => {
+                    res.should.have.status(200);
 
+                    expect(res.body.categories).to.be.an.instanceof(Array);
+
+                    res.body.should.be.a("object");
+
+                    done();
+                });
+        });
+    });
     /*
-    * Test the /GET route
+    * Test the /POST route
     */
     describe("/POST category", () => {
-    it("it should create (POST) new category", (done) => {
-      let category = {
-        title: "New category title 1",
-        description: "New category description 1",
-      };
-      chai
-        .request(app)
-        .post("/api/categories/create")
-        .set("x-access-token", token)
-        .send(category)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.body.should.be.a("object");
-          done();
+        it("it should create (POST) new category", (done) => {
+          let category = {
+            title: "New category title 1",
+            description: "New category description 1",
+          };
+          chai
+            .request(app)
+            .post("/api/categories/create")
+            .set('x-access-token', token)
+            .send(category)
+            .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a("object");
+              done();
+            });
         });
-    });
     });
 });
--- test/post.js
+++ test/post.js
@@ -8,38 +8,106 @@ import app from "../index.js";
 
 let should = chai.should();
 let expect = chai.expect;
+let token
 
 const Post = db.post;
 
 chai.use(chaiHttp);
 
-describe("Post", () => {
-    beforeEach((done) => {
-        //Before each test we empty the database
-        Post.remove({}, (err) => {
-          done();
-        });
+describe("Let's run the unit test for CRUD Post feature", () => {
+    //Before each test we empty the database
+   beforeEach((done) => {
+        let user = {
+            email: "admin@admin.com",
+            password: "123456789",
+        };
+
+        chai
+            .request(app)
+            .post("/api/auth/signin")
+            .send(user)
+            .end((err, res) => {
+                res.should.have.status(200);
+
+                res.body.should.be.a("object");
+
+                token = res.body.accessToken.token;
+            });
+        done()
     });
 
     /*
     * Test the /GET route
     */
-    describe("/GET post", () => {
-    it("it should GET all the posts", (done) => {
-          chai
-            .request(app)
-            .get("/api/posts")
-            .end((err, res) => {
-              res.should.have.status(200);
+    describe("As a registered user," +
+        "I want to view study-guides," +
+        "so that I can find the quickest way to learn what I want.", () => {
+        it("it should show (GET) all the posts", (done) => {
+              chai
+                .request(app)
+                .get("/api/posts")
+                .end((err, res) => {
+                  res.should.have.status(200);
 
-              expect(res.body.posts).to.be.an.instanceof(Array);
+                  expect(res.body.posts).to.be.an.instanceof(Array);
 
-              res.body.should.be.a("object");
+                  res.body.should.be.a("object");
 
-              // res.body.length.should.be.eql(0);
+                  // res.body.length.should.be.eql(0);
 
-              done();
+                  done();
+                });
             });
+        });
+
+    /*
+    * Test the /POST route with title and description
+    */
+    describe("As an admin user, " +
+        "I want to share a study guide," +
+        "so that I can share my knowledge.", () => {
+        it("it should create (POST) new post", (done) => {
+            let post = {
+                title: "New post title 1",
+                description: "New post description 1",
+                author: "Tester"
+            };
+            chai
+                .request(app)
+                .post("/api/posts/create")
+                .set('x-access-token', token)
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    done();
+                });
+        });
+    })
+
+    /*
+    * Test the /PATCH route with title and description
+    */
+    describe("As an admin user, " +
+        "I want to update my study guide, " +
+        "so that I can share the most updated resources with my clubmate.", () => {
+        it("it should edit (PATCH) an existing post", (done) => {
+            let test_id = "111aaa"
+            let post = {
+                title: "Edited title 1",
+                description: "Edited description 1",
+                author: "Tester"
+            };
+            chai
+                .request(app)
+                .patch("/api/posts/update/" + test_id)
+                .set('x-access-token', token)
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a("object");
+                    done();
+                });
         });
     });
 });
