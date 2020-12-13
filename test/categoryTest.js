43bef44a73691f72bac4a232a251bd69b15d974d
Buithienphuoc <tphuoc1998@gmail.com>
2020-12-14 01:11:41 +0700

    remove redundant packages and code add new comments into README.md refactor package.json

--- test/categoryTest.js
+++ test/categoryTest.js
@@ -9,7 +9,7 @@ let token;
 chai.use(chaiHttp);
 
 describe("Let's run unit test for CRUD Categories feature !!", () => {
-  //Before each test we empty the database
+  //Before each test we have to sign in (authentication) to get token
   beforeEach((done) => {
     let user = {
       email: "admin@admin.com",
@@ -20,7 +20,7 @@ describe("Let's run unit test for CRUD Categories feature !!", () => {
       .post("/api/auth/signin")
       .send(user)
       .end((err, res) => {
-        res.should.have.status(200);
+        res.should.have.status(200); // Means get successfully
         res.body.should.be.a("object");
         token = res.body.accessToken.token;
         done();
@@ -31,12 +31,13 @@ describe("Let's run unit test for CRUD Categories feature !!", () => {
    * Test the /GET route
    */
   describe("/GET category", () => {
+      // Test case: get all the categories
     it("it should show (GET) all the categories", (done) => {
       chai
         .request(app)
         .get("/api/categories")
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.status(200); // Means get successfully
           res.body.should.be.a("object");
 
           // res.body.length.should.be.eql(0);
@@ -46,13 +47,14 @@ describe("Let's run unit test for CRUD Categories feature !!", () => {
     });
   });
   describe("/GET categories by name", () => {
+      // Test case: get the category by the existed name
     it("it should show (GET) categories with the given name", (done) => {
       const mockCategory = "Mobile Development";
       chai
         .request(app)
         .get("/api/categories/" + mockCategory)
         .end((err, res) => {
-          res.should.have.status(200);
+          res.should.have.status(200); // means update successfully
 
           res.body.should.be.a("object");
 
@@ -64,6 +66,7 @@ describe("Let's run unit test for CRUD Categories feature !!", () => {
    * Test the /POST route
    */
   describe("/POST category", () => {
+      // Test case: create a new category with title and description are strings
     it("it should create (POST) new category", (done) => {
       let category = {
         title: "New category title 2",
@@ -75,10 +78,35 @@ describe("Let's run unit test for CRUD Categories feature !!", () => {
         .set("x-access-token", token)
         .send(category)
         .end((err, res) => {
-          res.should.have.status(201);
+          res.should.have.status(201); // means added successfully
           res.body.should.be.a("object");
           done();
         });
     });
   });
+    /*
+     * Test the /PATCH route
+     */
+    describe("As an admin user, " +
+        "I want to update category name," +
+        " so that user can recognize the new category easily", () => {
+        // Test case: update a category which is existed in the database
+        it("it should edit (PATCH) new category", (done) => {
+            let testId = "5fd636b137f3701556749c86"
+            let category = { // create a fake object to inject into the database
+                title: "Edited title",
+                description: "Edited title",
+            };
+            chai
+                .request(app)
+                .patch("/api/categories/update/" + testId)
+                .set("x-access-token", token)
+                .send(category)
+                .end((err, res) => {
+                    res.should.have.status(204); // means update successfully
+                    res.body.should.be.a("object");
+                    done();
+                });
+        });
+    });
 });
--- test/postTest.js
+++ test/postTest.js
@@ -11,7 +11,7 @@ chai.use(chaiHttp);
 describe("Let's run the unit test for CRUD Post feature", () => {
     //Before each test we empty the database
    beforeEach((done) => {
-        let user = {
+        let user = { // create a fake object to inject into database
             email: "admin@admin.com",
             password: "123456789",
         };
@@ -21,7 +21,7 @@ describe("Let's run the unit test for CRUD Post feature", () => {
             .post("/api/auth/signin")
             .send(user)
             .end((err, res) => {
-                res.should.have.status(200);
+                res.should.have.status(200); // Means get successfully
 
                 res.body.should.be.a("object");
 
@@ -36,12 +36,13 @@ describe("Let's run the unit test for CRUD Post feature", () => {
     describe("As a registered user," +
         "I want to view study-guides," +
         "so that I can find the quickest way to learn what I want.", () => {
+        // Test case: get all the posts
         it("it should show (GET) all the posts", (done) => {
               chai
                 .request(app)
                 .get("/api/posts")
                 .end((err, res) => {
-                  res.should.have.status(200);
+                  res.should.have.status(200); // Means get successfully
                   expect(res.body.posts).to.be.an.instanceof(Array);
                   res.body.should.be.a("object");
                   done();
@@ -55,6 +56,7 @@ describe("Let's run the unit test for CRUD Post feature", () => {
     describe("As an admin user, " +
         "I want to share a study guide," +
         "so that I can share my knowledge.", () => {
+        // Test case: create a new post without missing any element
         it("it should create (POST) new post", (done) => {
             let post = {
               title: "New post title 1",
@@ -69,7 +71,7 @@ describe("Let's run the unit test for CRUD Post feature", () => {
                 .set('x-access-token', token)
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(200);
+                    res.should.have.status(200); // Means get successfully
                     res.body.should.be.a("object");
                 });
             done();
@@ -83,6 +85,7 @@ describe("Let's run the unit test for CRUD Post feature", () => {
     describe("As an admin user, " +
         "I want to update my study guide, " +
         "so that I can share the most updated resources with my clubmate.", () => {
+        // Test case: edit a post existed in database
         it("it should edit (PATCH) an existing post", (done) => {
             let test_id = "5fd5ff9a49be26300febf862"
             let post = {
@@ -96,7 +99,7 @@ describe("Let's run the unit test for CRUD Post feature", () => {
                 .set('x-access-token', token)
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(200);
+                    res.should.have.status(200); // Means get successfully
                     res.body.should.be.a("object");
                 });
             done();
